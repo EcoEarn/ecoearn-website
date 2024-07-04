f4936a59c8394912b7be2d3cbac1f1a9675a8370
dudu <dudu@ecoearn.io>
2024-07-04 18:02:29 +0800

    feat: fix lint

--- src/types/network.ts
+++ src/types/network.ts
@@ -1,2 +1,2 @@
-export type BackEndNetworkType = 'dev' | 'test'| 'mainnet';
+export type BackEndNetworkType = 'dev' | 'test' | 'mainnet';
 export type NetworkItem = { cmsUrl: string; s3Url: string };
