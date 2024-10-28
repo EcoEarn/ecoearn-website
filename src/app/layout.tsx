137fddf2b08f9a26c647af208f4ecb73a4aa917f
Leon Zhang <leonard@ecoearn.io>
2024-10-28 19:37:25 +0800

    feat: update menu list

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from 'next';
 import '@/styles/globals.scss';
-import { getGlobalConfig } from '@/api/utils';
 import { GoogleAnalytics } from '@next/third-parties/google';
 import Layout from '@/pageComponents/layout';
-import { getHeader } from '@/api/utils';
+import { getGlobalConfig, getHeader, getFooter } from '@/api/utils';
 
 export async function generateMetadata(): Promise<Metadata> {
   const globalConfig = await getGlobalConfig();
@@ -17,13 +16,16 @@ export async function generateMetadata(): Promise<Metadata> {
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
   const headerData = await getHeader();
+  const footerData = await getFooter();
 
   const env = process.env.NEXT_PUBLIC_NETWORK_ENV;
   const googleAnalyticsId = env === 'mainnet' ? process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID || '' : '';
   return (
     <html lang="en">
       <body>
-        <Layout headerData={headerData}>{children}</Layout>
+        <Layout headerData={headerData} footerData={footerData}>
+          {children}
+        </Layout>
       </body>
       {googleAnalyticsId && <GoogleAnalytics gaId={googleAnalyticsId} />}
     </html>
